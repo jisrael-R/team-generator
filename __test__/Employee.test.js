b7613d6690305e2cf56d173c1939372d1da7d94e
Jorman <jorman.iisrael@me.com>
2021-10-11 16:09:47 -0400

    initial test

--- __test__/Employee.test.js
+++ __test__/Employee.test.js
@@ -7,3 +7,9 @@ test('create an Employee object',()=>{
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toEqual(expect.any(String));
 });
+
+test('gets employee name', ()=>{
+    const employee = new Employee('Jorman',2,'jorman.iisrael@me.com');
+
+    expect(employee.getName()).toBe('Jorman');
+})
